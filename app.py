77569411c7904bb6c18a3f82e6589c816b838e76
Bhuvnn <140882654+Bhuvnn@users.noreply.github.com>
2025-02-12 01:41:20 +0530

    new

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def send_email():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 9000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 9000)))
